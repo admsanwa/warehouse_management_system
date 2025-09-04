1d97e74ae9cb71616cfb00483381b90e90605b11
Danyisdev <danyxdevelop@gmail.com>
2025-09-04 22:52:06 +0700

    Fix due date filter productions

--- resources/views/api/production/list.blade.php
+++ resources/views/api/production/list.blade.php
@@ -32,7 +32,7 @@
                                     <div class="row">
                                         <div class="form-group col-md-2">
                                             <label for="">Date</label>
-                                            <input type="date" name="DocDate" class="form-control">
+                                            <input type="date" name="date" class="form-control">
                                         </div>
                                         <div class="form-group col-md-2">
                                             <label for="">IO No</label>
