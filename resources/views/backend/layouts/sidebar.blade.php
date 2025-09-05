11a64e2710909b1033a57cfe81c7fcd40b1494e3
Danyisdev <danyxdevelop@gmail.com>
2025-09-05 09:44:01 +0700

    Temporary disable inv trans menu

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -307,12 +307,7 @@
         @endif
 
         {{-- Inventory Tf --}}
-        @if (
-            $authDept == 'Quality Control' ||
-                $authDept == 'IT' ||
-                ($authDept == 'Procurement, Installation and Delivery' && $authLevel == 'Manager') ||
-                $authDept == 'Production and Warehouse' ||
-                $authDept == 'Production')
+        {{-- @if ($authDept == 'Quality Control' || $authDept == 'IT' || ($authDept == 'Procurement, Installation and Delivery' && $authLevel == 'Manager') || $authDept == 'Production and Warehouse' || $authDept == 'Production')
             <nav class="mt-2">
                 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                     data-accordion="false">
@@ -329,12 +324,7 @@
 
                         <ul class="collapse list-unstyled {{ Request::is('admin/inventorytf/*') ? 'show' : '' }} itemSubMenu"
                             id="inventorytfSubMenu">
-                            @if (
-                                $authDept == 'Procurement, Installation and Delivery' ||
-                                    $authDept == 'IT' ||
-                                    $authDept == 'Quality Control' ||
-                                    $authDept == 'Production and Warehouse' ||
-                                    $authDept == 'Production')
+                            @if ($authDept == 'Procurement, Installation and Delivery' || $authDept == 'IT' || $authDept == 'Quality Control' || $authDept == 'Production and Warehouse' || $authDept == 'Production')
                                 <li class="nav-item">
                                     <a href="{{ url('admin/inventorytf/create') }}"
                                         class="nav-link @if (Request::is('admin/inventorytf/create')) active @endif">
@@ -354,7 +344,7 @@
                     </li>
                 </ul>
             </nav>
-        @endif
+        @endif --}}
 
         {{-- Transactions --}}
         @if (
