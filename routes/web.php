789af6265b224b920c5fc973f3150c9f26af5521
Danyisdev <danyxdevelop@gmail.com>
2025-11-06 21:09:01 +0700

    Fix

--- routes/web.php
+++ routes/web.php
@@ -208,7 +208,7 @@ Route::group(['middleware' => 'admin'], function () {
     Route::get("admin/reports/finishgoods", [ReportsController::class, "finish_goods"]);
     Route::get("admin/reports/semifg", [ReportsController::class, "semifg"]);
     Route::get("/reports-bon", [ReportsController::class, 'bon']);
-    Route::get('/reports-bon-data', [ReportsController::class, 'data'])->name('report.bon.data');
+    Route::post('/reports-bon-data', [ReportsController::class, 'data'])->name('report.bon.data');
 
     // delivery
     Route::get("admin/delivery/list", [DeliveryController::class, "index"]);
