957f704fdbcd17e461de2a30c02a55de4a3e4ac9
Danyisdev <danyxdevelop@gmail.com>
2025-11-11 16:52:14 +0700

    Fix memo

--- resources/views/backend/production/memo.blade.php
+++ resources/views/backend/production/memo.blade.php
@@ -3,9 +3,14 @@
     <div class="content-wrapper">
         <div class="content-header">
             <div class="container-fluid">
-                <div class="row mb-2">
+                <div class="row mb-6">
                     <div class="col col-sm-6">
-                        <h1> Report Memo</h1>
+                        <h1>Memo</h1>
+                    </div>
+                    <div class="col col-sm-6">
+                        <ol class="breadcrumb justify-content-end">
+                            <a href="{{ url('admin/production/listmemo') }}" class="btn btn-primary btn-sm">List Memo</a>
+                        </ol>
                     </div>
                 </div>
             </div>
@@ -14,186 +19,142 @@
         <section class="content">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-md-12">
+                    <section class="col col-md-12">
                         <div class="card">
+                            @include('_message')
                             <div class="card-header">
-                                <h3 class="card-title">
-                                    Search List Memo
-                                </h3>
+                                <h3 class="card-title">Create Memo</h3>
                             </div>
-                            <form action="" method="get">
+                            <form action="{{ url('admin/production/memo') }}" class="form-horizontal" method="POST"
+                                enctype="multipart/form-data">
+                                @csrf
                                 <div class="card-body">
-                                    <div class="row">
-                                        <div class="form-group col-md-2">
-                                            <label for="">Dari Tanggal</label>
-                                            <input type="date" name="start_date" id="start_date" class="form-control">
-                                        </div>
-                                        <div class="form-group col-md-2">
-                                            <label for="">Sampai Tanggal</label>
-                                            <input type="date" name="end_date" id="end_date" class="form-control">
-                                        </div>
-                                        <div class="form-group col-md-3">
-                                            <button type="button" id="filter" class="btn btn-primary"
-                                                style="margin-top: 20px"><i class="fa fa-search"></i> Search</button>
-                                            <button type="button" id="reset" class="btn btn-warning"
-                                                style="margin-top: 20px"><i class="fa fa-eraser"></i> Reset</a>
+                                    <div class="form-group row">
+                                        <label for="" class="col-sm-3 col-form-lable">Nomer :</label>
+                                        <div class="col-sm-6">
+                                            <input name="no" id="no" value="{{ $number }}"
+                                                class="form-control" placeholder="Enter Nomer Terbit" readonly>
                                         </div>
                                     </div>
+                                    <div class="form-group row">
+                                        <label for="" class="col-sm-3 col-form-lable">Tanggal :</label>
+                                        <div class="col-sm-6">
+                                            <input type="date" name="date" id="date" class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="" class="col-sm-3 col-form-lable">Hal :</label>
+                                        <div class="col-sm-6">
+                                            <select name="description" id="description" class="form-control">
+                                                <option value="">Pilih hal disini</option>
+                                                <option value="Stock Produksi">Stock Produksi</option>
+                                                <option value="Kirim barang ke maklon">Kirim barang ke maklon</option>
+                                                <option value="Pengiriman material/komponen">Pengiriman material/komponen
+                                                </option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="" class="col-sm-3 col-form-label">Proyek/Order :</label>
+                                        <div class="col-sm-6">
+                                            <input type="text" name="project" id="project" class="form-control"
+                                                placeholder="Enter Proyek atau Order">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="" class="col-sm-3 col-form-label">IO :</label>
+                                        <div class="col-sm-6">
+                                            <input type="text" name="io" id="io" class="form-control"
+                                                placeholder="Enter IO">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="" class="col-sm-3 col-form-label">Due Date :</label>
+                                        <div class="col-sm-6">
+                                            <input type="date" name="duedate" id="duedate" class="form-control">
+                                        </div>
+                                    </div>
+                                    <div id="unitContainer">
+                                        <div class="unit-block">
+                                            <div class="form-group row">
+                                                <label for="" class="col-sm-3 col-form-label">Kebutuhan :</label>
+                                                <div class="col-sm-6">
+                                                    <input type="text" name="needs[]" class="form-control"
+                                                        placeholder="Enter Kebutuhan" required>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label for="" class="col-sm-3 col-form-lable">Unit :</label>
+                                                <div class="col-sm-2">
+                                                    <input type="text" name="unit[]" class="form-control"
+                                                        placeholder="Enter Unit">
+                                                </div>
+                                                <p>W :</p>
+                                                <div class="col-sm-1">
+                                                    <input type="number" name="width[]" class="form-control"
+                                                        placeholder="Enter Width">
+                                                </div>
+                                                <p>H :</p>
+                                                <div class="col-sm-1">
+                                                    <input type="number" name="height[]" class="form-control"
+                                                        placeholder="Enter Height">
+                                                </div>
+                                                <p>Qty :</p>
+                                                <div class="col-sm-1">
+                                                    <input type="number" name="qty[]" class="form-control"
+                                                        placeholder="Enter Quantity">
+                                                </div>
+                                                <p>Uom :</p>
+                                                <div class="col-sm-1">
+                                                    <input type="text" name="uom[]" class="form-control"
+                                                        placeholder="Enter Uom" list="uomList">
+                                                    <datalist id="uomList">
+                                                        <option value="Pcs">
+                                                        <option value="Unit">
+                                                        <option value="Lbr">
+                                                        <option value="Ltr">
+                                                        <option value="Btg">
+                                                        <option value="Set">
+                                                    </datalist>
+                                                </div>
+                                                <div class="col-sm-1 mt-1">
+                                                    <button type="button" class="btn btn-success btn-add-unit"><i
+                                                            class="fa fa-plus-circle"></i></button>
+                                                    <button type="button" class="btn btn-danger btn-remove-unit"
+                                                        style="display: none;"><i class="fa fa-minus-circle"></i></button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary"><i
+                                            class="fa fa-floppy-o"></i>Simpan</button>
                                 </div>
                             </form>
                         </div>
-
-                        @include('_message')
-                        <div class="card">
-                            <div class="card-header">
-                                <h3 class="card-title" id="titleDateRange">List From - To -</h3>
-                            </div>
-                            <div class="card-body">
-                                <div class="table-responsive">
-                                    <table id="memoTable" class="table table-bordered table-striped"></table>
-                                    <p></p>
-                                </div>
-                            </div>
-                            <div class="card-footer">
-                                <div class="d-flex justify-content-end px-2 py-2">
-                                    <div style="overflow-x: auto; max-width:100%">
-                                        {{-- {!! $getRecord->onEachSide(1)->appends(request()->except('page'))->links() !!} --}}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    </section>
                 </div>
             </div>
         </section>
     </div>
-@endsection
-@push('scripts')
-    <!-- jQuery (only one, do not load twice) -->
 
-    <!-- DataTables Core -->
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap4.min.css">
-    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap4.min.js"></script>
-
-    <!-- DataTables Buttons -->
-    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.bootstrap4.min.css">
-    <script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.bootstrap4.min.js"></script>
-
-    <!-- Excel Export Support -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
-
-    <!-- Print Support -->
-    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script>
-        $(function() {
-            let table = $("#memoTable").DataTable({
-                processing: true,
-                serverSide: true,
-                searching: false,
-                dom: 'Bfrtip', // 👈 Aktifkan button
-                buttons: [{
-                        extend: 'excelHtml5',
-                        title: '<h3>Report Memo</h3>',
-                        className: 'btn btn-success btn-sm mr-2',
-                        text: '<i class="fa fa-file-excel"</i> Export Excell',
-                    },
-                    {
-                        extend: 'print',
-                        title: '',
-                        className: 'btn btn-primary btn-sm',
-                        text: '<i class="fa fa-print"></i> Print',
-                        title: '', // kosongkan agar kita buat manual
-                        customize: function(win) {
+        $(document).on('click', '.btn-add-unit', function() {
+            // clone block
+            let $block = $(this).closest('.unit-block');
+            let $clone = $block.clone();
 
-                            let start = $('#start_date').val() ? $('#start_date').val() : '-';
-                            let end = $('#end_date').val() ? $('#end_date').val() : '-';
+            // clear values in the cloned inputs
+            $clone.find('input').val('');
+            $clone.find('.btn-remove-unit').show();
 
-                            $(win.document.body)
-                                .css('font-size', '14px')
-                                .prepend(`
-                                <div style="text-align:center; margin-bottom:20px;">
-                                    <h4 style="margin:0;">Report Memo</h4>
-                                    <p style="margin-top:5px;">Periode: ${start} s/d ${end}</p>
-                                    <hr style="border-top:1px solid #000; margin-top:10px;">
-                                </div>
-                            `);
-
-                            // Styling table agar lebih rapi
-                            $(win.document.body).find('table')
-                                .addClass('table table-bordered')
-                                .css({
-                                    'font-size': '12px',
-                                    'width': '100%',
-                                });
-                        }
-                    }
-                ],
-                ajax: {
-                    url: "{{ route('report.memo.data') }}",
-                    data: function(d) {
-                        d.start_date = $("#start_date").val();
-                        d.end_date = $("#end_date").val();
-                    }
-                },
-                columns: [{
-                        data: 'DT_RowIndex',
-                        title: 'No',
-                        orderable: false,
-                        searchable: false
-                    },
-                    {
-                        data: 'no',
-                        title: 'No Memo'
-                    },
-                    {
-                        data: 'date',
-                        title: 'Tanggal Memo'
-                    },
-                    {
-                        data: 'unit',
-                        title: 'Komponen'
-                    },
-                    {
-                        data: 'uom',
-                        title: 'Satuan'
-                    },
-                    {
-                        data: 'qty',
-                        title: 'Qty'
-                    }
-                    // { data: 'remark', title: 'Keterangan' },
-                    // { data: 'receipt_date', title: 'Tanggal Terima'}
-                    // { data: 'receipt_qty', title: 'Qty Terima'},
-                    // { data: 'remain_qty', title: 'Qty Sisa'},
-                    // { data: 'reason_qty', title: 'Alasan Qty Sisa'}
-                ]
-            });
-
-            $("#filter").click(function() {
-                let startDate = $("#start_date").val();
-                let endDate = $("#end_date").val();
-
-                function format(date) {
-                    return new Date(date).toLocaleDateString('id-ID', {
-                        day: '2-digit',
-                        month: '2-digit',
-                        year: 'numeric'
-                    });
-                }
-
-                if (startDate && endDate) {
-                    $("#titleDateRange").text("List From " + format(startDate) + " To " + format(endDate));
-                }
-                table.ajax.reload();
-            });
-            $("#reset").click(function() {
-                $('#start_date, #end_date, #no_memo').val('');
-                $("#titleDateRange").text("List From - To -");
-                table.ajax.reload();
-            });
+            // append clone to container
+            $('#unitContainer').append($clone);
+        });
+        $(document).on('click', '.btn-remove-unit', function() {
+            $(this).closest('.unit-block').remove();
         });
     </script>
-@endpush
+@endsection
